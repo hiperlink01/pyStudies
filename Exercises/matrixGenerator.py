6e35d5b3e488d5ec13577a38ae5f605f86283cdd
Oliver Limaverde <hiperlink01@hotmail.com>
2021-08-29 15:27:08 -0300

    Update matrixGenerator.py
    Added some simple analysis.

--- Exercises/matrixGenerator.py
+++ Exercises/matrixGenerator.py
@@ -7,14 +7,25 @@ matrix = [[0, 1, 2],
           [0, 1, 2]]
 
 for c in range(3):
-    for i in range(3):
-         matrix[c][i] = int(input(f'Insert a number in [{c}, {i}] '))
+    for i in range(3): 
+        matrix[c][i] = int(input(f'Insert a number in [{c}, {i}] '))
 
-print('-=-'*10)
+evenSum = 0
+col3sum = 0
+for c in matrix:
+    for column, i in enumerate(c):
+        if i % 2 == 0: evenSum += i
+        if column == 2: col3sum += i 
+
+print('-=-'*10, end='\n')
 
 print('3x3 MATRIX CREATED: \n')
 for c in matrix:
     for i in c: print(f'[{i:>5}]', end=' ')
     print('\n')
 
-print('Thank you very much!')
+print('-=-'*10)
+
+print('Sum of even numbers:', evenSum)
+print('Sum of the third column:', col3sum)
+print('Highest value of the second line:', max(matrix[1]))
